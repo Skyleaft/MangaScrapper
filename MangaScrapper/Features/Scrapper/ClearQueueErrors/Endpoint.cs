5ff98879c25a2a8e31ada326c83a73dfc344dcbb
Milzan Malik <milzan_malik@outlook.com>
2026-03-03 23:40:32 +0700

    feat: add ClearQueueErrors endpoint to clear scrapper queue errors
    - Introduced `/api/scrapper/queue/errors` endpoint with DELETE method.
    - Integrated `ClearErrorItems` from `BackgroundTaskQueue` for error management.
    - Enabled anonymous access for the endpoint.

---
+++ MangaScrapper/Features/Scrapper/ClearQueueErrors/Endpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using MangaScrapper.Infrastructure.BackgroundJobs;
+
+namespace MangaScrapper.Features.Scrapper.ClearQueueErrors;
+
+public class Endpoint(IBackgroundTaskQueue taskQueue) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Delete("/api/scrapper/queue/errors");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        taskQueue.ClearErrorItems();
+        await Send.NoContentAsync(ct);
+    }
+}
