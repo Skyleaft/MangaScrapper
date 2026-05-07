78030caddab0bd8c3db8da7cdf4ecc50e8d38af1
Milzan Malik <milzan_malik@outlook.com>
2026-05-07 22:50:05 +0700

    feat: move delete manga process to background job

--- MangaScrapper/Features/Manga/DeleteManga/Endpoint.cs
+++ MangaScrapper/Features/Manga/DeleteManga/Endpoint.cs
@@ -1,16 +1,12 @@
 using FastEndpoints;
+using Hangfire;
+using MangaScrapper.Infrastructure.BackgroundJobs;
 using MangaScrapper.Infrastructure.Repositories;
-using MangaScrapper.Infrastructure.Services;
-using Microsoft.Extensions.Options;
 
 namespace MangaScrapper.Features.Manga.DeleteManga;
 
-public class Endpoint(IMangaRepository mangaRepository,MeilisearchService meilisearchService, IOptions<ScrapperSettings> settings) : Endpoint<Request>
+public class Endpoint(IMangaRepository mangaRepository, IBackgroundJobClient jobClient) : Endpoint<Request>
 {
-    private readonly string _imageStoragePath = Path.IsPathRooted(settings.Value.ImageStoragePath)
-        ? settings.Value.ImageStoragePath
-        : Path.Combine(Directory.GetCurrentDirectory(), settings.Value.ImageStoragePath);
-
     public override void Configure()
     {
         Delete("/api/manga/{MangaId}");
@@ -25,37 +21,8 @@ public class Endpoint(IMangaRepository mangaRepository,MeilisearchService meilis
             return;
         }
 
-        var cleanTitle = GetCleanTitle(manga.Title);
-
-        foreach (var chapter in manga.Chapters)
-        {
-            var chapterDir = Path.Combine(_imageStoragePath, cleanTitle, chapter.Number.ToString());
-            if (Directory.Exists(chapterDir))
-            {
-                Directory.Delete(chapterDir, true);
-            }
-        }
-
-        if (!string.IsNullOrEmpty(manga.LocalImageUrl))
-        {
-            var thumbnailPath = Path.Combine(_imageStoragePath, manga.LocalImageUrl);
-            if (File.Exists(thumbnailPath))
-            {
-                File.Delete(thumbnailPath);
-            }
-        }
-
-        await mangaRepository.DeleteAsync(r.MangaId, ct);
-        await meilisearchService.DeleteMangaAsync( r.MangaId, ct);
+        jobClient.Enqueue<DeleteMangaJob>(job => job.ExecuteAsync(manga.Id, manga.Title, CancellationToken.None));
+        
         await Send.OkAsync(cancellation: ct);
-    }
-
-    private static string GetCleanTitle(string title)
-    {
-        var invalidChars = Path.GetInvalidFileNameChars()
-            .Union(new[] { '?', '*', ':', '|', '<', '>', '"' })
-            .ToArray();
-
-        return string.Concat(title.Split(invalidChars));
     }
 }
