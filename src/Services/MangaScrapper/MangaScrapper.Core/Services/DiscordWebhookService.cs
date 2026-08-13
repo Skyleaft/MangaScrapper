02f5005c751b9f189c7e3bbb34af25deb365b6f0
Milzan Malik <milzan_malik@outlook.com>
2026-08-14 01:06:29 +0700

    optimize memory for worker, fix image in discordWebhook

--- src/Services/MangaScrapper/MangaScrapper.Core/Services/DiscordWebhookService.cs
+++ src/Services/MangaScrapper/MangaScrapper.Core/Services/DiscordWebhookService.cs
@@ -54,7 +54,7 @@ public class DiscordWebhookService
                     .Split('/')
                     .Select(Uri.EscapeDataString));
 
-            var mangaThumb = string.IsNullOrWhiteSpace(manga.LocalImageUrl) ? null : new { url = $"{_domainSettings.DomainUrl}/api/images/{encodedPath}" };
+            var mangaThumb = string.IsNullOrWhiteSpace(manga.LocalImageUrl) ? null : new { url = $"{_domainSettings.DomainUrl}/images/{encodedPath}" };
 
             var embed = new
             {
